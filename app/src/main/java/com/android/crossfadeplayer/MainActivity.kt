4f0f815916e198028b3dd329fecbcd86bdaddc1e
Филипп <KrasnikovPhilipp@gmail.com>
2020-11-23 02:37:06 +0300

    Update for fadeIn / fadeOut, option to chose songs New color for seekbar

--- app/src/main/java/com/android/crossfadeplayer/MainActivity.kt
+++ app/src/main/java/com/android/crossfadeplayer/MainActivity.kt
@@ -1,29 +1,35 @@
 package com.android.crossfadeplayer
 
+import android.app.Activity
+import android.content.Intent
 import android.media.MediaPlayer
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
-import android.os.Handler
+import android.provider.MediaStore
 import android.util.Log
 import android.widget.SeekBar
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import java.lang.Exception
+
 
 class MainActivity : AppCompatActivity() {
+    private val REQUEST_CODE_1 = 1 //request code для pickAudioFile
+    private val REQUEST_CODE_2 = 2
 
-    //media player 1
-    private var mp: MediaPlayer? = null
-    //song list
-    private var currentSong = mutableListOf(R.raw.man,
-        R.raw.twice)
-    //song number
-    private var number = 0
-    //crossfade duration
-    var crossFadeTime = 2
+    private var mp1: MediaPlayer? = null
+    private var mp2: MediaPlayer? = null
 
+    private var song1:Uri? = null
+    private var song2:Uri? = null
+
+
+    private var number = 1 //номер проигрываемой песни
+
+    private var crossFadeTime = 2 //кр
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,28 +39,22 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun controlSound() {
-        songSwitch()
+        selectSong()
 
-        //press play to start playing default song
-        fb_play.setOnClickListener {
-
-            if (mp == null) {
-                mp = MediaPlayer.create(this, currentSong[number])
-                mp?.start()
-                crossFade()
-            }
+        fb_play.setOnClickListener {//play button
+                errorCheck()
         }
 
 
         //seekbar listener
-        sb_seekbar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+        sb_seekbar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
 
                 seekBar.max = 10
                 if (progress >= 2) {
                     crossFadeTime = progress
                 }
-                Log.d("Seekbar", "CrossFade:"+crossFadeTime)
+                Log.d("Seekbar", "CrossFade:" + crossFadeTime)
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
@@ -62,135 +62,125 @@ class MainActivity : AppCompatActivity() {
 
             override fun onStopTrackingTouch(seekBar: SeekBar?) {
             }
-
-
         })
+    }
+
+    fun errorCheck() {//проверка на ошибки
+        if (song1 !=null && song2 != null){
+            Log.d("mp","SONG1: $song1+"+"SONG2: $song2")
+            songCycle()
+            fb_play.isClickable = false
+            fb_1.isClickable = false
+            fb_2.isClickable = false
+        } else {
+            Toast.makeText(applicationContext, "Error: Select 2 audiofiles to play",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun songCycle() {//воспроизведение песен, в зависимости от текущего номера
+
+            if (number == 1) {
+                start1()
+            }
+            if (number == 2) {
+                start2()
+            }
 
     }
 
+    fun start1() {//запуск плеера+песни и fadeIn+fadeOut
+        mp1 = MediaPlayer.create(this, song1)
+        mp1?.start()
+
+        if (mp2 != null) {
+            fadeIn(mp1)
+        }
+
+        while (mp1 != null) {
+            if (mp1!!.currentPosition / 1000 > mp1!!.duration / 1000- crossFadeTime){
+                number = 2
+                fadeOut(mp1)
+            }
+        }
+    }
+
+    fun start2() {
+        mp2 = MediaPlayer.create(this, song2)
+        mp2?.start()
+
+        if (mp1 != null) {
+            fadeIn(mp2)
+        }
+
+        while (mp2 != null) {
+            if (mp2!!.currentPosition / 1000 > mp2!!.duration / 1000- crossFadeTime){
+                number = 1
+                fadeOut(mp2)
+            }
+        }
+    }
 
 
-
-    //press button = switch song
-    private fun songSwitch() {
+    private fun selectSong() {//выбор аудиофайла
         fb_1.setOnClickListener {
-            number = 0
-            Log.d("MA", "Current song is ${currentSong}")
+            pickAudioFile(REQUEST_CODE_1)
         }
         fb_2.setOnClickListener {
-            number = 1
-            Log.d("MA", "Current song is ${currentSong}")
+            pickAudioFile(REQUEST_CODE_2)
         }
     }
 
-
-
-    private fun crossFade() {
-        //media player 2
-        var mp2: MediaPlayer? = null
-        var secondsCount = 0
-        var playerCreated = false
-
-
-
-        //fade from low volume to max
-        fun fadeIn(mp: MediaPlayer?) {
-            var volumeNumber = 0.100f
-            mp?.setVolume(volumeNumber, volumeNumber)
-
-            GlobalScope.launch {
-
-                while (volumeNumber < 0.500f) {
-                    delay(100 * crossFadeTime.toLong())
-                    volumeNumber += 0.025f
-                    Log.d("FadeInVolume", "${volumeNumber}")
-                }
-            }
-        }
-
-        //fade from max volume to low
-        fun fadeOut(mp: MediaPlayer?, fadeFrom: Int) {
-            var volumeNumber = 0.500f
-            mp?.setVolume(volumeNumber, volumeNumber)
-
-
-            GlobalScope.launch {
-
-                while (volumeNumber > 0.050f) {
-                    delay(100 * crossFadeTime.toLong())
-                    volumeNumber -= 0.025f
-                    Log.d("FadeOutVolume", "${volumeNumber}")
-                }
-            }
-        }
-
-        //if number is not last choose nextSong
-        //else if number is last in list start from zero
-        fun nextSong() {
-            if (number + 1 < currentSong.size) {
-                number += 1
-            } else if (number + 1 == currentSong.size) {
-                number = 0
-            }
-        }
-
-        //release player when needed
-        fun release(mp: MediaPlayer?) {
-            if (mp !== null) {
-                mp.stop()
-                mp.reset()
-                mp.release()
-            }
-        }
-
-        /*release player when called
-        * choose next song
-        * start playing
-        * start fadingIn
-        * start fadingOut when time comes */
-        fun player2() {
-            release(mp2)
-            nextSong()
-            mp2 = MediaPlayer.create(this, currentSong[number])
-            mp2?.start()
-            fadeIn(mp2)
-            fadeOut(mp2, mp2!!.duration - crossFadeTime)
-            playerCreated = true
-        }
-
-        fun player1() {
-            release(mp)
-            nextSong()
-            mp = MediaPlayer.create(this, currentSong[number])
-            mp?.start()
-            fadeIn(mp)
-            fadeOut(mp, mp!!.duration - crossFadeTime)
-        }
-
+    fun fadeOut(mp: MediaPlayer?) {//постепенно уменьшает громкость песни
+        var volumeNumber = 1.0f
 
         GlobalScope.launch {
 
-            while (secondsCount >= 0 && mp !=null) {
-
-                //initialise player1 if seconds = song duration
-                if (secondsCount == mp!!.duration / 1000) {
-                    secondsCount = 0
-                    player1()
+            while (mp !=null && volumeNumber > 0.0f){
+                if (volumeNumber <= 0.1f) {
+                    mp.release()
+                    break
                 }
+                mp.setVolume(volumeNumber, volumeNumber)
+                delay(50 * crossFadeTime.toLong())
+                volumeNumber -= 0.1f
+            }
+        }
+        songCycle()
+    }
 
-                //initialise player2 if first player is about to finish song
-                if (secondsCount >= mp!!.duration / 1000 - crossFadeTime
-                    && !playerCreated
-                ) {
-                    player2()
+    fun fadeIn(mp: MediaPlayer?) {//постепенно увеличивает громкость песни
+        var volumeNumber = 0.1f
+
+        GlobalScope.launch {
+
+            while (volumeNumber < 1.0f){
+                mp?.setVolume(volumeNumber, volumeNumber)
+                delay(50 * crossFadeTime.toLong())
+                volumeNumber += 0.1f
+            }
+        }
+    }
+
+    private fun pickAudioFile(requestCode:Int) {
+        val intent = Intent(Intent.ACTION_GET_CONTENT, MediaStore.Audio.Media.EXTERNAL_CONTENT_URI)
+        intent.setType("audio/*")
+        startActivityForResult(intent, requestCode)
+    }
+
+   override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when(requestCode) {
+
+            REQUEST_CODE_1 -> {//песня №1 = песня выбранная после клика на №1
+                if (resultCode == Activity.RESULT_OK) {
+                    song1 = data!!.data
                 }
+            }
 
-                //delay 1second
-                delay(1000)
-
-                // count seconds of how much song is being played
-                secondsCount++
-                Log.d("SecondsCount", "${secondsCount}")
+            REQUEST_CODE_2 -> {//песня №2 = песня выбранная после клика на №2
+                if (resultCode == Activity.RESULT_OK) {
+                    song2 = data!!.data
+                }
             }
         }
     }
